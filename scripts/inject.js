f849f74ec16322a258aefbe00ee011377066154a
xteal <32860377+NoNoTeal@users.noreply.github.com>
2022-04-15 16:27:59 -1000

    Add files via upload

--- scripts/inject.js
+++ scripts/inject.js
@@ -2,12 +2,13 @@
 	try {
         var l = window.location.toString();
         if(l.startsWith("discord:///invite") || l.includes("discord.com/invite")) {
-		    var _ = document.createElement('script');
-		    _.src = chrome.extension.getURL('scripts/disable.js');
+			var _ = document.createElement('script');
+		    _.src = chrome.runtime.getURL('scripts/disable.js');
+			console.log(_.src);
 		    (document.head || document.documentElement).appendChild(_);
 		    _.onload = function() {
 		    	_.parentNode.removeChild(_);
 		    };
         }
-	} catch{}
+	} catch {}
 })();
